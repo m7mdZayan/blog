26a1c84fdfe0e17241da51a810e3dce2253fdbfe
unknown <m7mdzayan@gmail.com>
2021-03-20 01:00:27 +0200

    add all posts link in the navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,6 +55,12 @@
                                 </li>
                             @endif
                         @else
+                            <li>
+                                <a class="nav-link" href={{route('posts.index')}}>
+                                    All Posts
+                                </a>
+                            </li>
+
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
