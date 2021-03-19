4da24dd4aff8aca3e03190a4f2eb4afea736f8eb
unknown <m7mdzayan@gmail.com>
2021-03-20 00:36:35 +0200

    Authentication is added to all pages

--- routes/web.php
+++ routes/web.php
@@ -18,21 +18,21 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::get('posts/create',[\App\Http\Controllers\PostController::class,'create'])->name('posts.create');
+Route::get('posts/create',[\App\Http\Controllers\PostController::class,'create'])->name('posts.create')->middleware('auth');
 
-Route::get('posts/store',[\App\Http\Controllers\PostController::class,'store'])->name('posts.store');
+Route::get('posts/store',[\App\Http\Controllers\PostController::class,'store'])->name('posts.store')->middleware('auth');
 
-Route::get('/posts',[\App\Http\Controllers\PostController::class,'index'])->name('posts.index');
+Route::get('/posts',[\App\Http\Controllers\PostController::class,'index'])->name('posts.index')->middleware('auth');
 
-Route::put('post/{post}',[\App\Http\Controllers\PostController::class,'update'])->name('posts.update');
+Route::put('post/{post}',[\App\Http\Controllers\PostController::class,'update'])->name('posts.update')->middleware('auth');
 
-Route::get('/posts/{posts}',[\App\Http\Controllers\PostController::class,'show'])->name('posts.show');
+Route::get('/posts/{posts}',[\App\Http\Controllers\PostController::class,'show'])->name('posts.show')->middleware('auth');
 
-Route::get('/posts/{posts}/edit',[\App\Http\Controllers\PostController::class,'edit'])->name('posts.edit');
+Route::get('/posts/{posts}/edit',[\App\Http\Controllers\PostController::class,'edit'])->name('posts.edit')->middleware('auth');
 
-Route::get('/posts/{posts}/confirmdelete',[\App\Http\Controllers\PostController::class,'delete'])->name('posts.delete');
+Route::get('/posts/{posts}/confirmdelete',[\App\Http\Controllers\PostController::class,'delete'])->name('posts.delete')->middleware('auth');
 
-Route::get('/posts/{posts}/delete',[\App\Http\Controllers\PostController::class,'destroy'])->name('posts.destroy');
+Route::get('/posts/{posts}/delete',[\App\Http\Controllers\PostController::class,'destroy'])->name('posts.destroy')->middleware('auth');
 
 
 
